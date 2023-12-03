4fd2d6fcca0510ec52873a2eff3777746f491fd5
Fenilpanseriya <92420478+Fenilpanseriya@users.noreply.github.com>
2023-12-03 18:13:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,9 +19,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 dotenv.config();
-app.get("https://blog-editor-three.vercel.app/",(req,res)=>{
-  app.use(express.static(path.resolve(__dirname,"build")));
-  res.sendFile(path.resolve(__dirname,"build","index.html"));
+app.use(express.static(path.join(__dirname,"build")));
+app.get("/",(req,res)=>{
+  
+  res.sendFile(path.join(__dirname,"build","index.html"));
   
 })
 app.listen(process.env.PORT,()=>{
